f8d85969a984ae680238ba03bf2ed7495972bfa9
justin hook <justinhook@gmail.com>
2020-07-09 10:20:37 -0700

    add basic github auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.scss";
 
 function App() {
+  const [whichRepo, setWhichRepo] = useState("");
+  const [repos, setRepos] = useState([]);
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = () => {
+    if (login && password)
+      fetch("https://api.github.com/user/repos", {
+        method: "GET",
+        headers: { Authorization: "Basic " + btoa(`${user}:${password}`) },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setRepos(data);
+          console.log(data);
+        })
+        .catch(console.log);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <p>
+          <input
+            id="user"
+            value={user}
+            onChange={() =>
+              setUser(document.getElementById("user").value.trim())
+            }
+          />
+          <input
+            id="password"
+            value={password}
+            onChange={() =>
+              setPassword(document.getElementById("password").value.trim())
+            }
+          />
+          <button onClick={login} />
+        </p>
+
         <Counter />
         <p>
           Edit <code>src/App.js</code> and save to reload.
